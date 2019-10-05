849ee56461aa7157054c8270356950f35169b430
linsijiLaGuara <ycsilva9214@gmail.com>
2019-10-04 21:51:27 -0300

    Subiendo Codigo de contador

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Card } from 'semantic-ui-react';
 
-function App() {
+
+ export default class App extends React.Component{
+   state = { counter: 0 };
+
+    // decrement = ()=>this.setState({counter:this.state.counter-1});
+     decrement = () => this.setState(({ counter }) => ({ counter: counter - 1 }));
+
+     increment = () => this.setState(({ counter }) => ({ counter: counter + 1 }));
+
+     render() {
+       const { counter } = this.state;
+
   return (
     <div className="App">
+
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <Card>
+       <Card.Content>
+         <img src={logo} className="App-logo" alt="logo" />
+
+       <Card.Header>Contador Con React.js</Card.Header>
+
+       <Card.Description>
+
+
+              <h2>{counter}</h2>
+       </Card.Description>
+     </Card.Content>
+     <Card.Content extra>
+       <div className='ui two buttons'>
+       <Button basic color='green' onClick={this.increment}>+</Button>
+        <Button basic color='red' onClick={this.decrement}>-</Button>
+       </div>
+     </Card.Content>
+   </Card>
+   </header>
     </div>
   );
 }
-
-export default App;
+}
