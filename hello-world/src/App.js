b6744fa221affef6c31f71eaaadbdd88dccca854
linsijiLaGuara <ycsilva9214@gmail.com>
2019-10-10 21:10:24 -0300

    Actualizacion del codigo

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import { Button, Card } from 'semantic-ui-react';
 
 
  export default class App extends React.Component{
+
+
+
    state = { counter: 0 };
 
     // decrement = ()=>this.setState({counter:this.state.counter-1});
@@ -17,31 +21,27 @@ import { Button, Card } from 'semantic-ui-react';
 
   return (
     <div className="App">
-
-
       <header className="App-header">
-
         <Card>
        <Card.Content>
          <img src={logo} className="App-logo" alt="logo" />
-
        <Card.Header>Contador Con React.js</Card.Header>
-
        <Card.Description>
-
-
               <h2>{counter}</h2>
        </Card.Description>
      </Card.Content>
      <Card.Content extra>
-       <div className='ui two buttons'>
+      <div className='ui two buttons'>
        <Button basic color='green' onClick={this.increment}>+</Button>
         <Button basic color='red' onClick={this.decrement}>-</Button>
-       </div>
+      </div>
      </Card.Content>
    </Card>
+
    </header>
-    </div>
+
+
+       </div>
   );
 }
 }
